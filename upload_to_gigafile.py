12662a433f2914bb68a20b40b5c37ecf770627bf
Naoya-Wizard <79356057+Naoya-Wizard@users.noreply.github.com>
2024-01-13 20:26:33 +0900

    Create upload_to_gigafile.py

---
+++ upload_to_gigafile.py
@@ -0,0 +1,16 @@
+import sys
+from gigafile import GFile
+
+def upload_file(file_path):
+    # Gigafileにファイルをアップロードし、URLを取得
+    uploaded_file = GFile(file_path)
+    upload_response = uploaded_file.upload()
+    download_url = upload_response.get_download_page()
+
+    return download_url
+
+if __name__ == "__main__":
+    # コマンドライン引数から複数のファイルパスを取得
+    for file_path in sys.argv[1:]:
+        download_url = upload_file(file_path)
+        print(f"Uploaded file URL for {file_path}: {download_url}")
